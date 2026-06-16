94cc67cc260f10288e0c2e47241ee953a0572ddd
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-06-16 08:00:31 -0400

    inject the theme service into the application

--- practice/10. tech-blog-theming/src/app/app.component.ts
+++ practice/10. tech-blog-theming/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  readonly themeService = inject(ThemeService);
 }
